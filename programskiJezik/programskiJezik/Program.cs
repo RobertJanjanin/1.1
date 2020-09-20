f75f63ee8ac0baf364c270544127ae59f9a2b290
RobertJanjanin <55134583+RobertJanjanin@users.noreply.github.com>
2020-09-20 19:02:40 +0200

    Add files via upload

---
+++ programskiJezik/programskiJezik/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programskiJezik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Proceduralni x =  new Proceduralni();
+         Objektni y =  new Objektni();
+         Funkcionalni z =  new Funkcionalni();
+
+
+            Console.WriteLine("Upisite vrijednost postotka x: ");
+            Proceduralni=
+            Console.WriteLine("Upisite vrijednost postotka y: ");
+            Objektni=
+            Console.WriteLine("Upisite vrijednost postotka z: ");
+            Funkcionalni=
+            if (x, y, z = !100){ 
+            Console.WriteLine("Unijeli ste krivu kombinaciju")
+            }
+        }
+    }
+}
